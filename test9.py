164cdb63b543a7d9d6e60dfe845ff30d33fbef33
PST <pstmac@PSTs-MacBook-Pro-2.local>
2026-09-12 16:53:21 +0530

    assignment questions added

--- test9.py
+++ test9.py
@@ -52,3 +52,38 @@ print(l) # NameError: name 'l' is not defined
 
 # above errors you are getting because del will delete the object from your memory 
 # whose address that variable was storing
+
+
+# Assignment Questions for next 2 days --> solve and upload on group upto Sunday evening 13 sep 2026
+# Q1. STRING INDEXING
+
+s = "MachineLearning"
+
+# Find the output:
+
+print(s[s[2]])
+
+# Also explain why this works or gives an error.
+
+
+# Q2. LIST INDEXING
+
+numbers = [10, 20, [30, 40, [50, 60]], 70, 80]
+
+# Find the output:
+
+print(numbers[2][2][0])
+print(numbers[-3][1])
+print(numbers[2][-1][-2])
+
+
+# Q3. LIST SLICING
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Find the output:
+
+print(lst[8:2:-2])
+print(lst[-1:-9:-3])
+print(lst[2:8:-1])
+print(lst[-3:3:-2])
